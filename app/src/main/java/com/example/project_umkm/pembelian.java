a656009f11ea5cfd28b394d6d70cd5189afd3123
Ujang <apasihsh@gmail.com>
2024-10-04 20:43:12 +0700

    page 3 selesai page 4 buat

---
+++ app/src/main/java/com/example/project_umkm/pembelian.java
@@ -0,0 +1,14 @@
+package com.example.project_umkm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pembelian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.n4_activity_pembelian);
+    }
+}
