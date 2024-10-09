28f90e4354e671d99aad760eb03863bc96cfd963
Ujang <apasihsh@gmail.com>
2024-10-09 07:43:15 +0700

    page 3 selesai page 4 buat

---
+++ app/src/main/java/com/example/project_umkm/controller/n1_login_controller.java
@@ -0,0 +1,4 @@
+package com.example.project_umkm.controller;
+
+public class n1_login_controller {
+}
--- app/src/main/java/com/example/project_umkm/n1.java
+++ app/src/main/java/com/example/project_umkm/n1.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class login2 extends AppCompatActivity {
+public class n1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.n1_login);
+
     }
 }
